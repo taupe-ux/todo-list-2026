e2150f99147c73eaa81944f64b54eeb74752755f
taupe-ux <157597021+taupe-ux@users.noreply.github.com>
2026-05-31 21:37:40 +0900

    todolist-quotes-css

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 const logInForm = document.querySelector(".log-in");
 const logInInput = logInForm.querySelector("input");
 const logInPainting = document.querySelector(".painting");
+const logInHidden = document.querySelector(".main__log-in_greetings");
+
+const paintToDoDiv = document.querySelector(".main__todo");
 
 const USERNAME = "username";
 
@@ -11,12 +14,14 @@ function paintingLogIn(userName) {
   logInForm.classList.add(CLASSHIDDEN);
   logInPainting.classList.remove(CLASSHIDDEN);
   localStorage.setItem(USERNAME, userName);
+  paintToDoDiv.classList.remove(CLASSHIDDEN);
 }
 
 function handleLogIn(event) {
   event.preventDefault();
   const userName = logInInput.value;
   paintingLogIn(userName);
+  logInHidden.classList.add(CLASSHIDDEN);
 }
 
 logInForm.addEventListener("submit", handleLogIn);
@@ -24,5 +29,8 @@ logInForm.addEventListener("submit", handleLogIn);
 const SAVEDUSERNAME = localStorage.getItem(USERNAME);
 
 if (SAVEDUSERNAME !== null) {
+  paintToDoDiv.classList.remove(CLASSHIDDEN);
   paintingLogIn(SAVEDUSERNAME);
+} else {
+  logInHidden.classList.remove(CLASSHIDDEN);
 }
